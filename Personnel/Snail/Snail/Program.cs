f347cbccb508cc080685e03e176ea598b7c1f342
EthanRotzette <167179837+EthanRotzette@users.noreply.github.com>
2024-08-30 10:21:36 +0200

    Snail_fait

---
+++ Personnel/Snail/Snail/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int life = 50;
+            string snail = "_@_ö";
+            int left = 0;
+            
+            //déplacement
+            do
+            {
+                Thread.Sleep(100);
+                Console.Clear();
+                Console.SetCursorPosition(left, 15);
+                Console.WriteLine(snail);
+                life--;
+                left++;
+
+            } while (life >= 0);
+
+            //mort
+            Console.Clear();
+            Console.SetCursorPosition(left, 15);
+            Console.WriteLine("____");
+            Console.ReadLine();
+        }
+    }
+}
