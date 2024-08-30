0bddbc5df04833d57912c87dbd523d8f3aa8bbc8
EthanRotzette <167179837+EthanRotzette@users.noreply.github.com>
2024-08-30 12:04:32 +0200

    work_in_progress

---
+++ Personnel/ParaClub/ParaClub/Plane.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaClub
+{
+    internal class Plane
+    {
+        private string[] view =
+        {
+            @" _                         ",
+            @"| \                        ",
+            @"|  \       ______          ",
+            @"--- \_____/  |_|_\____  |  ",
+            @"  \_______ --------- __>-} ",
+            @"        \_____|_____/   |  "
+        };
+
+        public void View()
+        {
+            foreach (string line in view)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+    }
+}
---
+++ Personnel/ParaClub/ParaClub/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaClub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //définis la taille de l'écran
+            Console.BufferHeight = Config.SCREEN_HEIGHT;
+            Console.BufferWidth = Config.SCREEN_WIDTH;
+
+            Plane plane = new Plane();
+
+            plane.View();
+            Console.ReadLine();
+        }
+    }
+}
